3dab49f027518698fe2b4f6844fea61cfff5f9d7
esdwa <esdwa@LAPTOP-E7H21LK3>
2026-05-28 22:19:44 +0800

    Completed Get all fields, properties and methods from a class using reflection

---
+++ Reflection/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    public class Car
+    {
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year;
+        public void drive()
+        {
+            Console.WriteLine($"The {Make} {Model} is driving.");
+        }
+    }
+}
--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,12 +1,74 @@
 ﻿using Reflection;
+using System.Reflection;
 
-var annonymousField = typeof(SecretAgent).GetField("_codename", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while(true)
+        {
+            Console.WriteLine("Reflection Practice/Example:");
+            Console.WriteLine("1. Get Private Value and Change it");
+            Console.WriteLine("2. Get Method and Variable");
+            Console.WriteLine("Select an Option");
 
-var SecretAgentInstance = new SecretAgent();
+            string input = Console.ReadLine();
 
-var currentValue = annonymousField.GetValue(SecretAgentInstance);
-Console.WriteLine(currentValue);
+            if (!Enum.TryParse<Options>(input, out Options choice) || !Enum.IsDefined(typeof(Options), choice))
+            {
+                Console.WriteLine("Invalid Option");
+                continue;
+            }
 
-annonymousField.SetValue(SecretAgentInstance, "008");
-var newValue = annonymousField.GetValue(SecretAgentInstance);
-Console.WriteLine(newValue);
+            switch (choice)
+            {
+                case Options.GetPrivateValueAndChangeIt:
+                    var annonymousField = typeof(SecretAgent).GetField("_codename", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+                    var SecretAgentInstance = new SecretAgent();
+
+                    object? currentValue = annonymousField.GetValue(SecretAgentInstance);
+                    Console.WriteLine(currentValue);
+
+                    annonymousField.SetValue(SecretAgentInstance, "008");
+                    var newValue = annonymousField.GetValue(SecretAgentInstance);
+                    Console.WriteLine(newValue);
+
+                    break;
+
+                case Options.GetMethodAndVariable:
+                    Type type = typeof(Car);
+
+                    Console.WriteLine("Property Info: ");
+                    foreach(PropertyInfo prop in type.GetProperties())
+                    {
+                        Console.WriteLine($" -> {prop.PropertyType.Name} {prop.Name}");
+                    }
+
+                    Console.WriteLine(("Field Info: "));
+                    foreach(FieldInfo info in type.GetFields())
+                    {
+                        Console.WriteLine($" -> {info.FieldType.Name} {info.Name}");
+                    }
+
+                    Console.WriteLine("Method Info: ");
+                    foreach(MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        Console.WriteLine($" -> {method.ReturnType.Name} {method.Name}");
+                    }
+
+                    break;
+
+                case Options.GetPrivateValueAndChangeItAndRunPrivateMethod:
+                    break;
+            }
+        }
+    }
+
+    private enum Options
+    {
+        GetPrivateValueAndChangeIt = 1,
+        GetMethodAndVariable = 2,
+        GetPrivateValueAndChangeItAndRunPrivateMethod = 3
+    }
+}
