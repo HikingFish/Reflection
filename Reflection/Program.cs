b091333100bfcf2ea172119f47d970c29ebed16b
HikingFish <cheongwaikian1@gmail.com>
2026-05-28 21:23:32 +0800

    Initial File Upload
    completed some practice on how to get available variables or change variables that are private outside

---
+++ Reflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using Reflection;
+
+var annonymousField = typeof(SecretAgent).GetField("_codename", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+var SecretAgentInstance = new SecretAgent();
+
+var currentValue = annonymousField.GetValue(SecretAgentInstance);
+Console.WriteLine(currentValue);
+
+annonymousField.SetValue(SecretAgentInstance, "008");
+var newValue = annonymousField.GetValue(SecretAgentInstance);
+Console.WriteLine(newValue);
---
+++ Reflection/SecretAgent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    public class SecretAgent
+    {
+        private string _codename = "007";
+
+        public void DisplayCodename()
+        {
+            Console.WriteLine($"The secret agent's codename is: {_codename}");
+        }
+
+        public void DisplayCodeNameFormat2()
+        {
+            Console.WriteLine($"The secret agent in disguise's codename is: {_codename}");
+        }
+    }
+}
